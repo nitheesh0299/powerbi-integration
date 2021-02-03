02bed0256b1ac61f69991b0cb0b3ed5c83eb104c
SivaKesava Kuchi <64669814+MrKimmich@users.noreply.github.com>
2021-02-03 20:57:40 +0530

    Update powerbi_sessions_controller.rb

--- app/controllers/powerbi_sessions_controller.rb
+++ app/controllers/powerbi_sessions_controller.rb
@@ -6,38 +6,37 @@ class PowerbiSessionsController < ApplicationController
     BASE_URL   = "https://login.windows.net/9d5463e8-81bb-4342-8242-8e970dcbb974/oauth2/token"
     RESOURCE   = "https://analysis.windows.net/powerbi/api"
     def new
-        
+      if session[:user_id] != nil
+        #puts session[:user_id]
+      #   @username      = "bitspilanips@versaclouderp.com"
+      #   @password      = "BitsPilani123!"
+        @username      = params[:email]
+        @password      = params[:password]
+        ##@refresh_token = refresh_token
+        @client_id     = "c33afef1-d8a2-4ce7-8009-7866fd32a6df"
+        @client_secret = ".IQU-.mxg_m82323p57Fssv1VR1i7OKXq2"
+        @grant_type    = 'password'
+        @scope = 'openid'
+        #response = HTTParty.post(BASE_URL, build_body, build_headers)
+        response=HTTParty.post(BASE_URL,
+        :body => { :username=>@username, :password=>@password, :client_id=> @client_id, :client_secret=> @client_secret, :grant_type=> @grant_type, :resource=> RESOURCE, :scope=> @scope },
+        :headers => { 'Content-Type'=> 'application/x-www-form-urlencoded'})
+        data = response.body
+        @array = JSON.parse(data, symbolize_names: true)
+        #puts @array
+        #puts @array
+        #puts "next"
+        # puts session[:email_id]
+        # puts session[:company_id]
+        @access_token= @array[:access_token]
+        end
     end
   
     def create
-      if session[:user_id] != nil
-      #puts session[:user_id]
-    #   @username      = "bitspilanips@versaclouderp.com"
-    #   @password      = "BitsPilani123!"
-      @username      = $powerbi_mail
-      @password      = $powerbi_password
-      ##@refresh_token = refresh_token
-      @client_id     = "c33afef1-d8a2-4ce7-8009-7866fd32a6df"
-      @client_secret = ".IQU-.mxg_m82323p57Fssv1VR1i7OKXq2"
-      @grant_type    = 'password'
-      @scope = 'openid'
-      #response = HTTParty.post(BASE_URL, build_body, build_headers)
-      response=HTTParty.post(BASE_URL,
-      :body => { :username=>@username, :password=>@password, :client_id=> @client_id, :client_secret=> @client_secret, :grant_type=> @grant_type, :resource=> RESOURCE, :scope=> @scope },
-      :headers => { 'Content-Type'=> 'application/x-www-form-urlencoded'})
-      data = response.body
-      @array = JSON.parse(data, symbolize_names: true)
-      #puts @array
-      #puts @array
-      #puts "next"
-      # puts session[:email_id]
-      # puts session[:company_id]
-      @access_token= @array[:access_token]
-      end
+      
     end
   
     def destroy
 
     end
 end
-
