41826d13950dc17cdf0b159d7e880cb70f81b77a
nitheesh0299 <76263836+nitheesh0299@users.noreply.github.com>
2021-02-17 15:39:59 +0530

    added toggle for dialog

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module BiIntegration
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+     config.load_defaults 6.0
     config.session_store :active_record_store,
       :key => '_redmine_session'
     # Settings in config/environments/* take precedence over those specified here.
