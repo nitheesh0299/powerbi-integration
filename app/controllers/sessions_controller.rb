7e7f1448e6ea2fc5428d00c65b4260e520dbfc67
nitheesh0299 <76263836+nitheesh0299@users.noreply.github.com>
2021-04-06 13:56:28 +0530

    fixed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      session[:company_id] = user.companyId
+      session[:company_id] = user.companyid
       session[:role] = user.role
       puts session[:email_id]
       redirect_to root_url, notice: "Logged in!"
@@ -19,7 +19,6 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     session[:company_id] = nil
-    # $access_token=nil
     redirect_to root_url, notice: "Logged out!"
   end
 end
